d88998c9dac35c33d1aba632b6f644557f48acfc
sundaysare <115939897+sundaysare@users.noreply.github.com>
2022-10-16 19:27:11 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(100,150,20,30);
+  box.shapeColor = "green"'
+}
+
+function draw() 
+{ 
+  background(30);
+  drawSprites();
+}
+
+
+
+
